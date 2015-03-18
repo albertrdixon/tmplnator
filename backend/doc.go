d9bfd3572a497b43532bb85ede99facb0a01084a
albertrdixon <albert@timelinelabs.com>
2015-03-17 21:25:42 -0700

    Add doc.go files for godoc documentation

---
+++ backend/doc.go
@@ -0,0 +1,2 @@
+// Package backend defines an interface and implementations for accessing key/value stores
+package backend
---
+++ file/doc.go
@@ -0,0 +1,2 @@
+// Package file defines types and methods for working with template files
+package file
