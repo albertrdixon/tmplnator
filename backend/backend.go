cfbc7e0a4d446be5c9a2905b6c9030944da82cc8
albertrdixon <albert@timelinelabs.com>
2015-03-17 21:38:13 -0700

    Documentation for Backend

--- backend/backend.go
+++ backend/backend.go
@@ -1,10 +1,12 @@
 package backend
 
+// Backend types have methods for access key/value stores
 type Backend interface {
   Get(key string) (string, error)
   GetAll(key string) ([]string, error)
 }
 
+// New returns a Backend. If no peers are passed then returns nil
 func New(namespace string, peers []string) Backend {
   if len(peers) < 1 {
     return nil
@@ -12,6 +14,7 @@ func New(namespace string, peers []string) Backend {
   return newEtcd(namespace, peers)
 }
 
+// NewMock returns a mock Database
 func NewMock(vals map[string]string, valslice map[string][]string) Backend {
   return mockBackend{
     vals:     vals,
