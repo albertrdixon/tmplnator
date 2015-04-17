1832d08856b382f3c94cd7b9aec5df8e6d534637
albertrdixon <albert@timelinelabs.com>
2015-04-17 14:43:56 -0700

    Update t2.go to follow new style.

--- cmd/t2/t2.go
+++ cmd/t2/t2.go
@@ -1,71 +1,44 @@
 package main
 
 import (
-  "fmt"
-  l "github.com/Sirupsen/logrus"
-  "github.com/albertrdixon/tmplnator/config"
-  "github.com/albertrdixon/tmplnator/generator"
-  "github.com/ian-kent/gofigure"
-  "io/ioutil"
-  "os"
+	"os"
+	"path/filepath"
+
+	l "github.com/Sirupsen/logrus"
+	"github.com/albertrdixon/tmplnator"
+	"gopkg.in/alecthomas/kingpin.v1"
 )
 
-func setup() *config.Config {
-  var cfg = config.Defaults
-
-  // gofigure.Debug = true
-  err := gofigure.Gofigure(&cfg)
-  if err != nil {
-    fmt.Printf("Error parsing config: %v\n", err)
-    os.Exit(1)
-  }
-  registerLogger(cfg.Verbosity)
-
-  if cfg.ShowVersion {
-    fmt.Println(config.RuntimeVersion(config.CodeVersion, config.Build))
-    os.Exit(0)
-  }
-  if _, err := os.Stat(cfg.TmplDir); err != nil {
-    l.WithFields(l.Fields{
-      "directory": cfg.TmplDir,
-      "error":     err,
-    }).Fatal("Problems reading template dir")
-  }
-
-  if d, err := ioutil.TempDir("", "T2"); err == nil {
-    cfg.DefaultDir = d
-  }
-
-  return &cfg
-}
-
-func registerLogger(lvl int) {
-  l.SetOutput(os.Stdout)
-  switch {
-  case lvl <= 0:
-    l.SetLevel(l.ErrorLevel)
-  case lvl == 1:
-    l.SetLevel(l.InfoLevel)
-  case lvl >= 2:
-    l.SetLevel(l.DebugLevel)
-  }
-}
-
-func generateFiles(cfg *config.Config) (err error) {
-  g, err := generator.NewGenerator(cfg)
-  if err != nil {
-    return
-  }
-
-  err = g.Generate()
-  return
-}
+var (
+	debug       = kingpin.Flag("debug", "Enable debug mode").Short('d').Bool()
+	quiet       = kingpin.Flag("quiet", "Enable quiet mode").Short('q').Bool()
+	printTmpDir = kingpin.Flag("print-tmp", "Print out TmpDir").Short('p').Bool()
+	forceTmp    = kingpin.Flag("force-tmp", "Force all generated files to be written to TmpDir").Short('F').Bool()
+	tmpDir      = kingpin.Flag("tmpdir", "Set TmpDir").Default(filepath.Join(os.TempDir(), "T2")).OverrideDefaultFromEnvar("T2_TMPDIR").Short('T').String()
+	rootDir     = kingpin.Arg("template-directory", "Directory under which there are templates").Required().ExistingDir()
+)
 
 func main() {
-  cfg := setup()
+	kingpin.Version(tmplnator.Version)
+	kingpin.Parse()
 
-  if err := generateFiles(cfg); err != nil {
-    l.WithField("error", err).Fatal("ERROR!")
-  }
-  os.Exit(0)
+	l.SetOutput(os.Stdout)
+	l.SetLevel(l.InfoLevel)
+	if *quiet {
+		l.SetLevel(l.FatalLevel)
+	} else if *debug {
+		l.SetLevel(l.DebugLevel)
+	}
+
+	if *forceTmp {
+		tmplnator.ForceTemp = true
+	}
+	if *tmpDir != "" {
+		tmplnator.TmpDir = *tmpDir
+	}
+
+	tmplnator.NewGenerator(false, false).Generate(*rootDir)
+	if *printTmpDir {
+		println(tmplnator.TmpDir)
+	}
 }
