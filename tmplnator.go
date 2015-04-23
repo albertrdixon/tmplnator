e1ade45bcbd642a6d0b8d6133656e5f845d12b08
albertrdixon <albert@timelinelabs.com>
2015-04-23 10:38:18 -0700

    Pull entrypoint exported funcs out to tmplnator.go

---
+++ tmplnator.go
@@ -0,0 +1,52 @@
+package tmplnator
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+
+	l "github.com/Sirupsen/logrus"
+	"github.com/albertrdixon/tmplnator/backend"
+	"github.com/spf13/afero"
+)
+
+var (
+	errChan chan error
+	data    *Data
+	srcFs   afero.Fs
+	destFs  afero.Fs
+
+	// Backend is the Key/Value store for Data
+	Backend backend.Backend
+
+	// ForceTemp will force all templates to be written to TmpDir if true
+	ForceTemp = false
+
+	// TmpDir is the tmpdir we will use
+	TmpDir = filepath.Join(os.TempDir(), "T2")
+
+	cpus = runtime.NumCPU()
+	pkg  = "tmplnator"
+)
+
+func LogLevel(level string) {
+	lvl, err := l.ParseLevel(level)
+	if err != nil {
+		lvl = l.InfoLevel
+	}
+	l.SetLevel(lvl)
+}
+
+func MemGen(root string) []*File {
+	initFs(false, true)
+	return generate(root)
+}
+
+func RealGen(root string) []*File {
+	initFs(false, false)
+	return generate(root)
+}
+
+func ClearFs() {
+	srcFs, destFs = nil, nil
+}
